acd01de706c7db66515fc6c13a71ecb67f5bcf14
Jagger <634750802@qq.com>
2024-12-23 15:29:19 +0800

    support update partial chat engine

--- e2e/tests/chat-engine.spec.ts
+++ e2e/tests/chat-engine.spec.ts
@@ -1,4 +1,4 @@
-import { expect, type Page, test } from '@playwright/test';
+import { expect, type Locator, type Page, test } from '@playwright/test';
 import { loginViaApi } from '../utils/login';
 
 test.describe('Chat Engine', () => {
@@ -128,11 +128,71 @@ test.describe('Chat Engine', () => {
         await checkChatEngineAvailability(page, name);
       });
     });
+
+    test('Update', async ({ page }) => {
+      await test.step('Goto page', async () => {
+        await loginViaApi(page);
+        await page.goto('/chat-engines');
+        await page.getByRole('button', { name: 'New Chat Engine' }).click();
+        await page.waitForURL('/chat-engines/new');
+
+        const name = 'Chat Engine to be updated';
+
+        await test.step('Fill in fields', async () => {
+          // Fill in name
+          await page.getByRole('textbox', { name: 'Name' }).fill(name);
+
+          // Goto retrieval tab
+          await page.getByRole('tab', { name: 'Retrieval' }).click();
+
+          // Select default knowledge base
+          await selectOption(page, 'Select Knowledge Base', /My Knowledge Base/);
+        });
+
+        const chatEngineId = await test.step('Create', async () => {
+          await page.getByRole('button', { name: 'Create Chat Engine' }).click();
+          await page.waitForURL(/\/chat-engines\/\d+$/);
+
+          const [_, idString] = /\/chat-engines\/(\d+)$/.exec(page.url());
+          return parseInt(idString);
+        });
+
+        await page.goto('/chat-engines/' + chatEngineId);
+
+        await test.step('Update Name', async () => {
+          await page.getByRole('textbox', { name: 'Name' }).fill('Chat Engine to be updated (updated)');
+          await waitUpdate(page, page.getByRole('textbox', { name: 'Name', disabled: false }));
+
+          expect(await getChatEngine(page, chatEngineId).then(ce => ce.name)).toBe('Chat Engine to be updated (updated)');
+        });
+
+        await test.step('Update LLM', async () => {
+          await selectOption(page, 'LLM', /My LLM/);
+          await waitUpdate(page, page.getByRole('button', { name: 'LLM', exact: true, disabled: false }));
+
+          expect(await getChatEngine(page, chatEngineId).then(ce => ce.llm_id)).toBe(1);
+        });
+
+        await page.getByRole('tab', { name: 'Retrieval' }).click();
+        await test.step('Update KG Depth', async () => {
+          await page.getByRole('spinbutton', { name: 'Depth' }).fill('3');
+          await waitUpdate(page, page.getByRole('spinbutton', { name: 'Depth', disabled: false }));
+
+          expect(await getChatEngine(page, chatEngineId).then(ce => ce.engine_options.knowledge_graph.depth)).toBe(3);
+        });
+
+        // TODO: add cases for rest fields
+      });
+    });
   });
 });
 
 // TODO: The selectors are tricky. Update the select component to simplify the validation.
 async function checkChatEngineAvailability (page: Page, name: string) {
+  await page.locator('[data-sidebar="menu"] li').filter({ hasText: /Chat Engines/ }).getByRole('link').click();
+  // wait for chat engine table updated.
+  await page.getByText(name).waitFor();
+
   await page.goto('/');
 
   // Select the 'Select Chat Engine' combobox
@@ -160,4 +220,10 @@ async function selectOption (page: Page, name: string, value: string | RegExp) {
   await page.getByRole('button', { name: name, exact: true }).click();
   await page.getByRole('option', { name: value }).click();
   await expect(page.getByRole('button', { name: name, exact: true })).toHaveText(value);
+}
+
+async function waitUpdate (page: Page, locator: Locator) {
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('button', { name: 'Save' }).waitFor({ state: 'detached' });
+  await locator.waitFor();
 }

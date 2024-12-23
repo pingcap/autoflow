bcee1f8c8401f67d4460b618c7c2cfb718b8abb0
Jagger <634750802@qq.com>
2024-12-23 14:42:46 +0800

    support create with featured configuration

--- e2e/tests/chat-engine.spec.ts
+++ e2e/tests/chat-engine.spec.ts
@@ -21,9 +21,7 @@ test.describe('Chat Engine', () => {
         await page.getByRole('tab', { name: 'Retrieval' }).click();
 
         // Select default knowledge base
-        await page.getByRole('button', { name: 'Select Knowledge Base' }).click();
-        await page.getByRole('option', { name: 'My Knowledge Base' }).click();
-        await expect(page.getByRole('button', { name: 'Select Knowledge Base' })).toHaveText(/My Knowledge Base/);
+        await selectOption(page, 'Select Knowledge Base', /My Knowledge Base/);
       });
 
       const chatEngineId = await test.step('Create', async () => {
@@ -51,14 +49,90 @@ test.describe('Chat Engine', () => {
       });
 
       await test.step('Check availability', async () => {
-        await checkChatEngineAvailability(page, chatEngineId, name);
+        await checkChatEngineAvailability(page, name);
+      });
+    });
+
+    test('Create with featured configuration', async ({ page }) => {
+      await test.step('Goto page', async () => {
+        await loginViaApi(page);
+        await page.goto('/chat-engines');
+        await page.getByRole('button', { name: 'New Chat Engine' }).click();
+        await page.waitForURL('/chat-engines/new');
+      });
+
+      const name = 'Featured configuration';
+
+      await test.step('Fill in fields', async () => {
+        // Fill in name
+        await page.getByRole('textbox', { name: 'Name' }).fill(name);
+
+        // Set LLM & Fast LLM
+        await selectOption(page, 'LLM', /My LLM/);
+        await selectOption(page, 'Fast LLM', /My LLM/);
+        // TODO: Create a Fast LLM in place
+
+        // Goto retrieval tab
+        await page.getByRole('tab', { name: 'Retrieval' }).click();
+
+        // Select default knowledge base
+        await selectOption(page, 'Select Knowledge Base', /My Knowledge Base/);
+
+        // Select Reranker
+        await selectOption(page, 'Reranker', /My Reranker/);
+
+        // KG Properties
+        await page.getByRole('spinbutton', { name: 'Depth' }).fill('1'); // Do not use 2 for default value is 2
+        await page.getByRole('checkbox', { name: 'Include Meta' }).check();
+        await page.getByRole('checkbox', { name: 'Using intent search' }).check();
+
+        // Goto features tab
+        await page.getByRole('tab', { name: 'Features' }).click();
+
+        await page.getByRole('checkbox', { name: 'Hide Reference Sources' }).check();
+        await page.getByRole('switch', { name: 'Clarify Question' }).check();
+      });
+
+      const chatEngineId = await test.step('Create', async () => {
+        await page.getByRole('button', { name: 'Create Chat Engine' }).click();
+        await page.waitForURL(/\/chat-engines\/\d+$/);
+
+        const [_, idString] = /\/chat-engines\/(\d+)$/.exec(page.url());
+        return parseInt(idString);
+      });
+
+      await test.step('Validate configurations', async () => {
+        // Validate chat engine configurations
+        const chatEngine = await getChatEngine(page, chatEngineId);
+        expect(chatEngine.name).toBe(name);
+        expect(chatEngine.engine_options).toStrictEqual({
+          knowledge_base: {
+            linked_knowledge_base: {
+              id: 1,
+            },
+          },
+          knowledge_graph: {
+            depth: 1,
+            include_meta: true,
+            using_intent_search: true,
+          },
+          hide_sources: true,
+          clarify_question: true,
+        });
+        expect(chatEngine.llm_id).toBe(1);
+        expect(chatEngine.fast_llm_id).toBe(1);
+        expect(chatEngine.reranker_id).toBe(1);
+      });
+
+      await test.step('Check availability', async () => {
+        await checkChatEngineAvailability(page, name);
       });
     });
   });
 });
 
 // TODO: The selectors are tricky. Update the select component to simplify the validation.
-async function checkChatEngineAvailability (page: Page, id: number, name: string) {
+async function checkChatEngineAvailability (page: Page, name: string) {
   await page.goto('/');
 
   // Select the 'Select Chat Engine' combobox
@@ -80,4 +154,10 @@ async function checkChatEngineAvailability (page: Page, id: number, name: string
 async function getChatEngine (page: Page, id: number) {
   const ceResponse = await page.request.get(`/api/v1/admin/chat-engines/${id}`);
   return await ceResponse.json();
+}
+
+async function selectOption (page: Page, name: string, value: string | RegExp) {
+  await page.getByRole('button', { name: name, exact: true }).click();
+  await page.getByRole('option', { name: value }).click();
+  await expect(page.getByRole('button', { name: name, exact: true })).toHaveText(value);
 }

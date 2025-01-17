29629fa3d363121d9a26b68b5acb1f37bf708944
Jagger <634750802@qq.com>
2025-01-17 17:59:16 +0800

    feat(frontend): support js new chat api in widget

--- frontend/app/src/components/chat/chat-hooks.tsx
+++ frontend/app/src/components/chat/chat-hooks.tsx
@@ -6,7 +6,7 @@ import type { AppChatStreamState, StackVMState } from '@/components/chat/chat-st
 import { useGtagFn } from '@/components/gtag-provider';
 import { useBootstrapStatus } from '@/components/system/BootstrapStatusProvider';
 import { useLatestRef } from '@/components/use-latest-ref';
-import { createContext, type ReactNode, useContext, useEffect, useState } from 'react';
+import { createContext, type ReactNode, type Ref, useContext, useEffect, useImperativeHandle, useState } from 'react';
 
 export interface ChatsProviderValues {
   chats: Map<string, ChatController>;
@@ -30,7 +30,7 @@ const ChatsContext = createContext<ChatsProviderValues>({
 
 const ChatControllerContext = createContext<ChatController | null>(null);
 
-export function ChatsProvider ({ children }: { children: ReactNode }) {
+export function ChatsProvider ({ newChatRef, children }: { newChatRef?: Ref<ChatsProviderValues['newChat'] | undefined>, children: ReactNode }) {
   const bootstrapStatusRef = useLatestRef(useBootstrapStatus());
   const [chats, setChats] = useState(() => new Map<string, ChatController>);
 
@@ -54,6 +54,8 @@ export function ChatsProvider ({ children }: { children: ReactNode }) {
       return chats;
     });
   };
+
+  useImperativeHandle(newChatRef, () => newChat);
 
   return (
     <ChatsContext.Provider value={{
--- frontend/packages/widget-react/src/Widget.tsx
+++ frontend/packages/widget-react/src/Widget.tsx
@@ -1,7 +1,7 @@
 import type { BootstrapStatus } from '@/api/system';
 import { ManualScrollVoter } from '@/components/auto-scroll';
 import { AutoScroll } from '@/components/auto-scroll/auto-scroll';
-import { ChatsProvider } from '@/components/chat/chat-hooks';
+import { ChatsProvider, type ChatsProviderValues } from '@/components/chat/chat-hooks';
 import { Conversation } from '@/components/chat/conversation';
 import { useGtagFn } from '@/components/gtag-provider';
 import { PortalProvider } from '@/components/portal-provider';
@@ -108,6 +108,8 @@ export const Widget = forwardRef<WidgetInstance, WidgetProps>(({ container, trig
     }
   }, [trigger]);
 
+  const newChatRef = useRef<ChatsProviderValues['newChat'] | undefined>(undefined);
+
   useImperativeHandle(ref, () => ({
     get open () {
       return openRef.current;
@@ -125,13 +127,19 @@ export const Widget = forwardRef<WidgetInstance, WidgetProps>(({ container, trig
       setDark(d);
     },
     get initialized (): true { return true; },
+    newChat (content: string) {
+      newChatRef.current?.(undefined, [], {
+        content,
+        chat_engine: chatEngine,
+      });
+    },
   }), []);
 
   return (
     <PortalProvider container={container}>
       <BootstrapStatusProvider bootstrapStatus={bootstrapStatus}>
         <ExperimentalFeaturesProvider features={experimentalFeatures}>
-          <ChatsProvider>
+          <ChatsProvider newChatRef={newChatRef}>
             <Dialog open={open} onOpenChange={(open) => {
               setOpen(open);
               if (!open) {
@@ -165,9 +173,7 @@ export const Widget = forwardRef<WidgetInstance, WidgetProps>(({ container, trig
                         Ask AI
                       </span>
                     </DialogTitle>
-                    <DialogDescription className="sr-only">
-                      .
-                    </DialogDescription>
+                    <DialogDescription className="sr-only" />
                   </DialogHeader>
                   <AutoScroll target={scrollTarget} edgePixels={12}>
                     <ManualScrollVoter />

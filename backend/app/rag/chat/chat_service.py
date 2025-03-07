8106dca93de3d2bfce6ca30bdbd402ec06a735e3
nagao <nagaopx@gmail.com>
2025-03-07 15:50:13 +0800

    fix: incorrect column name

--- backend/app/rag/chat/chat_service.py
+++ backend/app/rag/chat/chat_service.py
@@ -243,7 +243,7 @@ def check_rag_config_need_migration(session: Session) -> NeedMigrationStatus:
         .where(ChatEngine.deleted_at == None)
         .where(
             text(
-                "JSON_EXTRACT(engine_options, '$.knowledge_base.linked_knowledge_base') IS NULL"
+                "JSON_EXTRACT(engine_options, '$.knowledge_base.linked_knowledge_bases') IS NULL"
             )
         )
     )

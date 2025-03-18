a574e7e9d9655033fc200dd5e211e165fc6ce87f
Mini256 <minianter@foxmail.com>
2025-03-18 14:22:18 +0800

    fix: fix the TableModel path in the rag

--- core/autoflow/storage/memory_store/base.py
+++ core/autoflow/storage/memory_store/base.py
@@ -3,10 +3,10 @@ from typing import List, Dict
 
 from sqlmodel import Field
 from tidb_vector.sqlalchemy import VectorType
-from autoflow.storage.tidb.base import TiDBModel
+from autoflow.storage.tidb.schema import TableModel
 
 
-class Memory(TiDBModel, table=True):
+class Memory(TableModel, table=True):
     __tablename__ = "agent_memory"
     id: uuid.UUID = Field(None, primary_key=True)
     vector: List[float] = Field(None, sa_type=VectorType)
